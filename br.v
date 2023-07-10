677813793ec9947263dd377a6738085b74cc253b
changhz <chenghc@live.fr>
2023-07-09 22:05:19 -0400

    br

--- br.v
+++ br.v
@@ -1,6 +1,9 @@
-import os
+import os {execute}
 import flag
-import lib.input {str_input, f64_input}
+
+const (
+	data_root = $env('DATA_ROOT')
+)
 
 fn main() {
 	mut fprs := flag.new_flag_parser(os.args)
@@ -9,10 +12,9 @@ fn main() {
 	fprs.description('Bible Reader')
 	fprs.skip_executable()
 
-	reserve := fprs.bool('reserve', `r`, false, 'Reserve a table')
-
-	mut name := fprs.string('name', `n`, '', 'Your name')
-	mut nguests := fprs.int('nguests', `a`, -1, 'Number of guests')
+	// reserve := fprs.bool('reserve', `r`, false, 'Reserve a table')
+	mut version := fprs.string('version', `v`, 'wlc', 'Bible version')
+	// mut book := fprs.string('name', `n`, '', 'Your name')
 
 	additional_args := fprs.finalize() or {
 		eprintln(err)
@@ -20,29 +22,28 @@ fn main() {
 		return
 	}
 
-	if reserve {
-		println('Table reservation service.')
+	books := "
+		gen, exo, lev, num, deu, jos, jdg, rut,
+		1sa, 2sa, 1ki, 2ki, 1ch, 2ch, ezr, neh,
+		est, job, psa, pro, ecc, sng, isa, jer,
+		lam, ezk, dan, hos, jol, amo, oba, jon,
+		mic, nam, hab, zep, hag, zec, mal
+	".trim(' ')
 
-		if name == '' {
-			name = str_input("What's your name?")
-		}
+	pages := [
+		050, 040, 027, 036, 034, 024, 021, 004,
+		031, 024, 022, 025, 029, 036, 010, 013,
+		010, 042, 150, 031, 012, 008, 066, 052,
+		005, 048, 012, 014, 004, 009, 001, 004,
+		007, 003, 003, 003, 002, 014, 003
+	]
 
-		if nguests == -1 {
-			num, abort := f64_input("How many are you?")
-			if abort { return }
-			nguests = int(num)
-		}
+	booklist := books.split(',').map(it.trim(' \n\t'))
 
-		if name == input.abort_key {
-			println('You chose not to tell us your name.')
-		} else {
-			println('Your name is $name and')
-		}
+	list := execute("ls $data_root/$version")
+	files := list.output.split('\n')
 
-		println('you are $nguests.str() persons')
-		println('A table has been reserved for you, thank you.')
-		return
-	}
+	println(files)
 
 	println(additional_args.join_lines())
 	println(fprs.usage())
