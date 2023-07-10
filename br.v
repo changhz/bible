f6fc3bb57872d5d7ee32338eee65ca7e0f8858c5
changhz <chenghc@live.fr>
2023-07-09 23:37:11 -0400

    read file

--- br.v
+++ br.v
@@ -1,8 +1,9 @@
-import os {execute}
+import os
 import flag
 
 const (
 	data_root = $env('DATA_ROOT')
+	alefbet = '׃ אבגדהוזחטיכלמנסעפצקרשתםןףץך'
 )
 
 fn main() {
@@ -12,9 +13,8 @@ fn main() {
 	fprs.description('Bible Reader')
 	fprs.skip_executable()
 
-	// reserve := fprs.bool('reserve', `r`, false, 'Reserve a table')
+	listbooks := fprs.bool('list', `l`, false, 'List books')
 	mut version := fprs.string('version', `v`, 'wlc', 'Bible version')
-	// mut book := fprs.string('name', `n`, '', 'Your name')
 
 	additional_args := fprs.finalize() or {
 		eprintln(err)
@@ -28,7 +28,12 @@ fn main() {
 		est, job, psa, pro, ecc, sng, isa, jer,
 		lam, ezk, dan, hos, jol, amo, oba, jon,
 		mic, nam, hab, zep, hag, zec, mal
-	".trim(' ')
+	"
+
+	if listbooks {
+		println(books)
+		return
+	}
 
 	pages := [
 		050, 040, 027, 036, 034, 024, 021, 004,
@@ -40,11 +45,42 @@ fn main() {
 
 	booklist := books.split(',').map(it.trim(' \n\t'))
 
-	list := execute("ls $data_root/$version")
-	files := list.output.split('\n')
+	if additional_args.len < 2 {
+		println("Usage: br BOOK CHAPTER [VERSES] [OPTIONS]")
+		return
+	}
 
-	println(files)
+	book := additional_args[0]
+	chapter := additional_args[1]
+	if additional_args.len == 3 {
+		verses := additional_args[2]
+	}
+
+	if !booklist.contains(book) {
+		eprintln("Invalid book name '$book'")
+		return
+	}
+
+	mut path := "$data_root/$version/"
+	list := os.execute("ls $path | awk '/${book.to_upper()}/ && /$chapter/'")
+	file := list.output.trim(' \n')
+
+	path += file
+	txt := os.read_file(path)!
+	cleaned := clean(txt.split('\n')[2..].join('\n'))
+	println(cleaned.join('').trim(' '))
 
 	println(additional_args.join_lines())
 	println(fprs.usage())
+}
+
+
+fn clean(txt string) []string {
+	mut cleaned := []string{}
+	for line in txt.split('\n') {
+		for ch in line.runes() {
+			if alefbet.contains(ch.str()) {cleaned << ch.str()}
+		}
+	}
+	return cleaned
 }
